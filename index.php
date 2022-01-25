f17e48afc574679f85376f2cebba2bf49570d3d1
Lucas Costa Rodrigues <84423749+lucas-crodrigues@users.noreply.github.com>
2022-01-25 18:09:41 -0300

    Added title, description, and survey-form

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <title></title>
   </head>
   <body>
-    
+    <h1 id="title">Survey</h1>
+    <p id="description">Today we're doing a survey on cats</p>
+    <form id="survey-form"></form>
   </body>
 </html>
