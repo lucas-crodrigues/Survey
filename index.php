594ca97cf780212ecc7a0b951ea3a5bf830eddc7
benyessefb <82568893+benyessefb@users.noreply.github.com>
2022-01-25 23:24:26 +0200

    add the name and email

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
   <body>
     <h1 id="title">Survey</h1>
     <p id="description">Today we're doing a survey on cats</p>
-    <form id="survey-form"></form>
+    <form id="survey-form">
+      <label for="name">First name:</label><br>
+      <input type="text" id="name" name="name" value=""><br>
+      <label for="email">Email:</label><br>
+      <input type="text" id="email" name="email" value=""><br>
+      <input type="submit" value="Submit">
+    </form>
   </body>
 </html>
